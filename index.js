e16a00088d70836c4ed32506a84a2f7124c4bf19
Halyna-K <hkzlvs@gmail.com>
2021-12-10 14:10:36 +0200

    done part 3, part 4( add list)

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hi world!")
+const chalk = require('chalk');
+const { Command } = require('commander');
+const { listContacts, getContactById, removeContact, addContact } = require('./contacts')
+
+const program = new Command();
+program
+  .requiredOption('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+     const list = await listContacts()
+     console.table(list);
+      break;
+
+    case 'get':
+      // ... id
+      break;
+
+    case 'add':
+      // ... name email phone
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn(chalk.red('Unknown action type!'));
+  }
+}
+
+invokeAction(argv).then(()=> console.log('Operation success'))
